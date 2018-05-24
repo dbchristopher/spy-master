69ed461ba4baa9718a7efafca739dcd7c36c6a58
Daniel Christopher <dbchristopher@gmail.com>
2018-05-23 19:44:34 -0700

    spacing

--- src/cljs/spy_master/components/card.cljs
+++ src/cljs/spy_master/components/card.cljs
@@ -1,6 +1,7 @@
 (ns spy-master.components.card
   (:require [reagent.core :as r]))
 
+; State management
 (def is-visible (r/atom false))
 
 (defn toggle-state [current-state]
@@ -8,12 +9,10 @@
     false
     true))
 
+; Display
 (defn word-card [word]
-  [
-    :div.word-card
-    {
-      :class (str (if @is-visible "blue" "default"))
-      :on-click #(swap! is-visible toggle-state )
-    }
+  [:div.word-card
+    { :class (str (if @is-visible "blue" "default"))
+      :on-click #(swap! is-visible toggle-state )}
     word
   ])
